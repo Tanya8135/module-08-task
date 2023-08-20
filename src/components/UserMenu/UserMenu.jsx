7f9ba487e59ae130b7dcef6615eec0887a9aa3f4
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-08-20 15:57:10 +0300

    fixed comment in UserMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -6,7 +6,7 @@ import css from './UserMenu.module.css';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
-  const { user } = useAuth(); /* це зімість useSelector(selectUser) */
+  const { user } = useAuth(); /* це замість useSelector(selectUser) */
 
   return (
     <div className={css.wrapper}>
